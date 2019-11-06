a1e55b95057f5aa1c9f87e21fcb26f0bb4bfe47f
pedro-pablo <pedropablopolanco@hotmail.com>
2019-11-05 22:43:44 -0400

    Alteração do .NET Core para 3.0; cadastro de usuário e anúncio (para apresentação)

---
+++ CarrosMotosBob/Controllers/AnunciosController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CMBData;
+using CMBData.Models;
+
+namespace CarrosMotosBob.Controllers
+{
+    public class AnunciosController : Controller
+    {
+        private readonly CMBContext _context;
+
+        public AnunciosController(CMBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Anuncios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Anuncios.ToListAsync());
+        }
+
+        // GET: Anuncios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Anuncios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ano,Descricao,Status")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(anuncio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(anuncio);
+        }
+
+        #region Edit e Remove
+        /*
+
+        // GET: Anuncios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncios.FindAsync(id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ano,Descricao,Status")] Anuncio anuncio)
+        {
+            if (id != anuncio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(anuncio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnuncioExists(anuncio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var anuncio = await _context.Anuncios.FindAsync(id);
+            _context.Anuncios.Remove(anuncio);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        
+        private bool AnuncioExists(int id)
+        {
+            return _context.Anuncios.Any(e => e.Id == id);
+        }
+        */
+
+        #endregion
+    }
+}
